4088e8a2f56d3af0829ac17c1832cb09e6d04b01
jatin <jatn.gupta.cd.mat22@itbhu.ac.in>
2023-10-11 00:55:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const btn = document.querySelector(".btn");
+
+const updateLSData = () => {
+    const textAreaData = document.querySelectorAll("textarea");
+    const notes = [];
+    console.log(textAreaData);
+    textAreaData.forEach((note) => {
+      return notes.push(note.value);
+    });
+    console.log(notes);
+  
+    localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+const addnewnote = (text ='')=>{
+    const note = document.createElement('div');
+    note.classList.add('note');
+    const htmlData = `
+    <div class="operation">
+        <button class="edit"> <i class="fas fa-edit"></i> </button>
+        <button class="delete"> <i class="fas fa-trash-alt"></i> </button>
+    </div>
+
+    <div class="main ${text? "" :"hidden"}  "> </div>
+    <textarea class="${text? "hidden" : ""}"></textarea>  `;
+
+    var notessec = document.querySelector(".notes")
+
+    note.insertAdjacentHTML("afterbegin", htmlData);
+    notessec.appendChild(note)
+    console.log(note);
+    const main = note.querySelector(".main");
+    const textarea = note.querySelector("textarea")
+
+    textarea.value = text;
+    main.innerHTML = text;
+
+    const delnote = note.querySelector(".delete")
+    delnote.addEventListener("click",()=>{
+        note.remove();
+        updateLSData();
+    })
+    
+    const editnote = note.querySelector(".edit");
+    editnote.addEventListener("click",()=>{
+        main.classList.toggle("hidden");
+        textarea.classList.toggle("hidden");
+    })
+
+    textarea.addEventListener("change", (event) => {
+        const value = event.target.value;
+        main.innerHTML = value;
+        updateLSData();
+    });
+
+}
+
+const notes = JSON.parse(localStorage.getItem("notes"));
+
+if (notes) {
+  notes.forEach((note) => addnewnote(note));
+}
+
+btn.addEventListener("click", () => addnewnote());
